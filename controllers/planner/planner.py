76839c17a70d90525fe2d76d15f61be90ad4355e
Joshua J. Damanik <joshuajdmk@gmail.com>
2022-11-22 21:28:52 +0900

    Initial commit

---
+++ controllers/planner/planner.py
@@ -0,0 +1,60 @@
+from controller import Robot
+from controller import Motor
+from controller import InertialUnit
+from controller import GPS
+from controller import Gyro
+from controller import Keyboard
+from controller import Camera
+from controller import DistanceSensor
+from controller import Supervisor
+
+from math import cos, sin, degrees, radians
+import sys
+import numpy as np
+
+sys.path.append('../..')
+
+from firmware import cffirmware as firm
+
+robot = Robot()
+
+timestep = int(robot.getBasicTimeStep())
+
+## Initialize motors
+m1_motor = robot.getDevice("m1_motor")
+m1_motor.setPosition(float('inf'))
+m1_motor.setVelocity(-1)
+m2_motor = robot.getDevice("m2_motor")
+m2_motor.setPosition(float('inf'))
+m2_motor.setVelocity(1)
+m3_motor = robot.getDevice("m3_motor")
+m3_motor.setPosition(float('inf'))
+m3_motor.setVelocity(-1)
+m4_motor = robot.getDevice("m4_motor")
+m4_motor.setPosition(float('inf'))
+m4_motor.setVelocity(1)
+
+
+## Initialize Sensors
+imu = robot.getDevice("inertial unit")
+imu.enable(timestep)
+gps = robot.getDevice("gps")
+gps.enable(timestep)
+Keyboard().enable(timestep)
+gyro = robot.getDevice("gyro")
+gyro.enable(timestep)
+camera = robot.getDevice("camera")
+camera.enable(timestep)
+range_front = robot.getDevice("range_front")
+range_front.enable(timestep)
+range_left = robot.getDevice("range_left")
+range_left.enable(timestep)
+range_back = robot.getDevice("range_back")
+range_back.enable(timestep)
+range_right = robot.getDevice("range_right")
+range_right.enable(timestep)
+
+past_time = robot.getTime()
+
+# core
+firm.plan_takeoff()
